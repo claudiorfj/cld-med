705020e2e6d15ab4c61cc51bd4ec0378715a88c9
Cláudio Fernandes <93228350+claudiorfj@users.noreply.github.com>
2023-05-09 14:37:04 +0100

    create get List Doctors

--- api/src/main/java/med/cld/api/controller/DoctorsController.java
+++ api/src/main/java/med/cld/api/controller/DoctorsController.java
@@ -1,6 +1,9 @@
 package med.cld.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,7 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.cld.api.doctor.Doctor;
 import med.cld.api.doctor.DoctorRepository;
+import med.cld.api.doctor.ListDoctorData;
 import med.cld.api.doctor.RegisterDoctorData;
 
 @RestController
@@ -23,6 +27,11 @@ public class DoctorsController {
   @Transactional
   public void register(@RequestBody @Valid RegisterDoctorData data) {
     repository.save(new Doctor(data));
+  }
+
+  @GetMapping
+  public List<ListDoctorData> listing() {
+    return repository.findAll().stream().map(ListDoctorData::new).toList();
   }
 
 }
