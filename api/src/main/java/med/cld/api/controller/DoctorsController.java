30aa80f063efc42cd7f2febcddaa1f38f69cef00
Cláudio Fernandes <93228350+claudiorfj@users.noreply.github.com>
2023-05-09 12:09:57 +0100

    Update DoctorsController.java

--- api/src/main/java/med/cld/api/controller/DoctorsController.java
+++ api/src/main/java/med/cld/api/controller/DoctorsController.java
@@ -1,19 +1,25 @@
 package med.cld.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import med.cld.api.doctor.Doctor;
+import med.cld.api.doctor.DoctorRepository;
 import med.cld.api.doctor.RegisterDoctorData;
 
 @RestController
 @RequestMapping("/doctors")
 public class DoctorsController {
+
+  @Autowired
+  private DoctorRepository repository;
   
   @PostMapping
   public void register(@RequestBody RegisterDoctorData data) {
-    System.out.println(data);
+    repository.save(new Doctor(data));
   }
 
 }
