6a7e5b1b69f2a1b7f95f76edd80b3a03fbdb0d0d
Cláudio Fernandes <93228350+claudiorfj@users.noreply.github.com>
2023-05-09 16:35:53 +0100

    add PageableDefault

--- api/src/main/java/med/cld/api/controller/DoctorsController.java
+++ api/src/main/java/med/cld/api/controller/DoctorsController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,7 +33,7 @@ public class DoctorsController {
   }
 
   @GetMapping
-  public Page<ListDoctorData> listing(Pageable pagination) {
+  public Page<ListDoctorData> listing(@PageableDefault(size = 10, sort = {"name"}) Pageable pagination) {
     return repository.findAll(pagination).map(ListDoctorData::new);
   }
 
