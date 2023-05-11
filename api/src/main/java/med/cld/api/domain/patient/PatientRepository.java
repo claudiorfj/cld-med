ac64c18a325234129cd89a09d9966a36524d7457
Cláudio Fernandes <93228350+claudiorfj@users.noreply.github.com>
2023-05-11 11:48:36 +0100

    add findActiveById

--- api/src/main/java/med/cld/api/domain/patient/PatientRepository.java
+++ api/src/main/java/med/cld/api/domain/patient/PatientRepository.java
@@ -2,10 +2,18 @@ package med.cld.api.domain.patient;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PatientRepository extends JpaRepository<Patient, Long>{
   
   Page<Patient> findAllByActiveTrue(org.springframework.data.domain.Pageable pagination);
+
+  @Query("""
+    select p.active
+    from Patient p
+    where p.id = :id
+    """)
+  Boolean findActiveById(Long idPatient);
 
 
 }
