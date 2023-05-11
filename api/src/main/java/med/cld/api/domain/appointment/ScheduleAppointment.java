b0f058547cf65f4c74b08c9a4761ce411d822ba9
Cláudio Fernandes <93228350+claudiorfj@users.noreply.github.com>
2023-05-11 12:55:07 +0100

    implement validator

--- api/src/main/java/med/cld/api/domain/appointment/ScheduleAppointment.java
+++ api/src/main/java/med/cld/api/domain/appointment/ScheduleAppointment.java
@@ -1,9 +1,12 @@
 package med.cld.api.domain.appointment;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import med.cld.api.domain.ExceptionValidation;
+import med.cld.api.domain.appointment.validations.ValidatorAppointmentSchedule;
 import med.cld.api.domain.doctor.Doctor;
 import med.cld.api.domain.doctor.DoctorRepository;
 import med.cld.api.domain.patient.PatientRepository;
@@ -20,13 +23,17 @@ public class ScheduleAppointment {
   @Autowired
   private PatientRepository patientRepository;
 
-  public void toSchedule (ScheduleAppointmentData data) {
-    if(!patientRepository.existsById(data.idPatient())){
+  private List<ValidatorAppointmentSchedule> validators;
+
+  public void toSchedule(ScheduleAppointmentData data) {
+    if (!patientRepository.existsById(data.idPatient())) {
       throw new ExceptionValidation("ID Patient not found");
     }
-    if(data.idDoctor() != null && !doctorRepository.existsById(data.idDoctor())){
+    if (data.idDoctor() != null && !doctorRepository.existsById(data.idDoctor())) {
       throw new ExceptionValidation("ID Doctor not found");
     }
+    validators.forEach(v -> v.validate(data));
+    
     var doctor = doctorRepository.getReferenceById(data.idDoctor());
     var patient = selectDoctor(data);
     var appointment = new Appointment(null, doctor, patient, data.date());
@@ -34,16 +41,14 @@ public class ScheduleAppointment {
   }
 
   private Doctor selectDoctor(ScheduleAppointmentData data) {
-    if(data.idDoctor() != null) {
+    if (data.idDoctor() != null) {
       return doctorRepository.getReferenceById(data.idDoctor());
     }
-
     if (data.specialty() == null) {
       throw new ExceptionValidation("Specialty is mandatory if you don't choose a doctor");
     }
-
     return doctorRepository.chooseFreeDoctor(data.specialty(), data.date());
 
   }
-  
+
 }
