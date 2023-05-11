27c7bbc63281c150e1227287bf274bdfb6648f82
Cláudio Fernandes <93228350+claudiorfj@users.noreply.github.com>
2023-05-11 11:49:50 +0100

    add validators

--- api/src/main/java/med/cld/api/domain/appointment/AppointmentRepository.java
+++ api/src/main/java/med/cld/api/domain/appointment/AppointmentRepository.java
@@ -1,7 +1,13 @@
 package med.cld.api.domain.appointment;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long>{
+
+  boolean existsByPatientIdAndDateBetween(Long idPatient, LocalDateTime firstSchedule, LocalDateTime lastSchedule);
+
+  boolean existsByDoctorIdAndDate(Long idDoctor, LocalDateTime date);
   
 }
